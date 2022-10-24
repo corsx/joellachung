48893e4e666f1ff8fc38f68a9a91639fa93925ef
corsx <97646343+corsx@users.noreply.github.com>
2022-10-24 14:25:29 -0400

    using lightgallery

--- js/main.js
+++ js/main.js
@@ -120,8 +120,4 @@ $grid.imagesLoaded().progress( function() {
 
 
 
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
- 
+
